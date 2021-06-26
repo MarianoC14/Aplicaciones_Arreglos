b80ec038ef3da1f5be43adfa40cc4731336bd1b8
MarianoC14 <86450013+MarianoC14@users.noreply.github.com>
2021-06-25 23:56:33 -0600

    Programa Veinte_Números
    De igual manera en este programa se utilizaron los arreglos para almacenar en cada uno los 20 números que se solicitaran al usuario para posteriormente ordenarlos y mostrar estos números ordenados.

---
+++ Veinte_Numeros/Veinte_Numeros/Numeros.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Veinte_Numeros
+{
+    class Numeros
+    {
+        //------------------Atributos-------------------
+        double num;
+        string ordenamiento;
+        //-----------------Array--------------- 
+        public double[] ArrayNumero = new double[20];//--20 números
+        //---Metodo para Solicitar al usuario que ingrese un número---
+        public void asignar_Numero()
+        {
+            for(int x = 0; x < 20; x++)
+            {
+                Console.Clear();//Limpiar consola para no mostrar 20 veces el mismo proceso en pantalla. 
+                Console.WriteLine($"-----------NUMERO {x + 1}------------");
+                Console.Write(">> Introduce un Número: ");
+                num = Convert.ToDouble(Console.ReadLine());
+                ArrayNumero[x] = num;
+            }
+        }
+        //----Método para ordenar los Números Ingresados----
+        public string ordenar_Numeros()
+        {
+            ConsoleKeyInfo Ordenar;//Instancia para mi variable que identificara la tecla presionada.
+            do {
+                Console.WriteLine("-----Presiona Enter para Ordenar tus números----");
+                Ordenar = Console.ReadKey(true);
+            } while (Ordenar.Key != ConsoleKey.Enter);//Bucle que me solicitara que presione enter hasta que lo haga. 
+            Array.Sort(ArrayNumero);//Ordenar todos los números en el array de menor a mayor. 
+            for(int i = 0; i < 20; i++)
+            {
+                ordenamiento += $"-- {ArrayNumero[i]} ";//Mostrar Todos los números ordenados. 
+            }
+            return ordenamiento;
+        }
+    }
+}
---
+++ Veinte_Numeros/Veinte_Numeros/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Veinte_Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numeros objNumeros = new Numeros();
+            ConsoleKeyInfo repetir_programa;
+            do
+            {
+                Console.WriteLine("-------------20 NUMEROS---------------");
+                Console.ReadKey();
+                objNumeros.asignar_Numero();
+                Console.WriteLine(objNumeros.ordenar_Numeros());
+                Console.ReadKey();
+                Console.WriteLine("____________________________________________");
+                Console.WriteLine("<<PRESIONA [R] SI DESEAS REPETIR EL PROGRAMA>>");
+                repetir_programa = Console.ReadKey();
+            } while(repetir_programa.Key == ConsoleKey.R);
+                Console.WriteLine(">>>>>>GRACIAS POR UTILIZAR EL PROGRAMA<<<<<<");
+        }
+    }
+}
