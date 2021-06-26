9fb6a49c05c1fb7829328a8baf01d9d3d0f28f9e
MarianoC14 <86450013+MarianoC14@users.noreply.github.com>
2021-06-25 23:49:19 -0600

    Programa Cadena_Caracteres
    Programa que en base a una palabra dada se realizan diferentes acciones tales como invertir la palabra, convertirla en mayúscula y en base a las características o indicaciones de la descripción del programa, se utilizo arreglos para almacenar carácter por carácter (letra por letra) para realizar dichas modificaciones.

---
+++ Cadena_Caracteres/Cadena_Caracteres/Palabra.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cadena_Caracteres
+{
+    class Palabra
+    {
+        //--------------Atributos---------------
+        string palabra, palabra_Invertida, mayuscula;
+        int tamaño;
+        //-------------Propiedades--------------
+        public string PALABRA
+        {
+            get { return this.palabra; }
+            set { this.palabra = value;}
+        }
+        //---------------Métodos----------------
+        //<><><><><><Invertir Palabra<><><><><><
+        public string Invertir_Palabra(string palabra)
+        {
+            PALABRA = palabra; //------Asignacion de la variable
+            tamaño = PALABRA.Length; //------Valor del tamaño de la palabra
+            for (int x = tamaño - 1; x >= 0; x--)//---Ciclo que recorre la palabra
+            {
+                palabra_Invertida += PALABRA[x];//--Asignacion de Caracteres iniciando desde el ultimo
+            }
+            return palabra_Invertida;
+        }
+        //<><><><>Convertir en Mayuscula<><><><<
+        public string Hacer_Mayuscula(string palabra)
+        {
+            PALABRA = palabra;
+            mayuscula = PALABRA.ToUpper();//Hacer la palabra en Mayuscula
+            return mayuscula;
+        }
+        //<><><><><>Convertir en Minuscula<><><><><><
+        public string Hacer_Minuscula(string palabra)
+        {
+            string minuscula;
+            PALABRA = palabra;
+            minuscula = PALABRA.ToLower();//Hacer la palabra en Minuscula
+            return minuscula;
+        }
+        //<><><><Hacer Modificaciones<><><><><><><><><
+        public string Correccion_de_Caracteres(string palabra)
+        {
+            //-----------Atributos del metodo----------
+            string palabra_Modificada = "";
+            int i = 0;
+            PALABRA = palabra;
+            tamaño = PALABRA.Length;
+            char[] letra = new char[tamaño];//Arreglo para cada letra
+            foreach(var l in mayuscula)//Ciclo para agregar letra por letra al arreglo
+            {
+                letra[i] = (l);
+                i++;
+            }
+            for(int x = 0; x < tamaño; x++)//Ciclo para recorrer todos los Arreglos
+            { 
+                switch(letra[x])//Condisiones de Cambios
+                {
+                    case 'A': letra[x] = '4'; break;
+                    case 'I': letra[x] = '1'; break;
+                    case 'S': letra[x] = '5'; break;
+                    case 'B': letra[x] = '8'; break;
+                    case 'E': letra[x] = '3'; break;
+                    case 'T': letra[x] = '7'; break;
+                }
+            }
+            for(int x = 0; x < tamaño; x++)
+            {
+                palabra_Modificada += letra[x];
+            }
+            return palabra_Modificada;
+        }
+    }
+}
---
+++ Cadena_Caracteres/Cadena_Caracteres/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Cadena_Caracteres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //---------Instancia de la clase----------
+            Palabra objPalabra = new Palabra();
+            Console.Write(">>PALABRA: ");
+            objPalabra.PALABRA = Console.ReadLine();
+            Console.WriteLine("_______________________________");
+            Console.WriteLine("-------------------------------");
+            Console.Write(">>PALABRA INVERTIDA: ");
+            Console.WriteLine(objPalabra.Invertir_Palabra(objPalabra.PALABRA));
+            Console.WriteLine("_______________________________");
+            Console.WriteLine("-------------------------------");
+            Console.Write(">>MAYUSCULAS: ");
+            Console.WriteLine(objPalabra.Hacer_Mayuscula(objPalabra.PALABRA));
+            Console.WriteLine("_______________________________");
+            Console.WriteLine("-------------------------------");
+            Console.Write(">>MINUSCULAS: ");
+            Console.WriteLine(objPalabra.Hacer_Minuscula(objPalabra.PALABRA));
+            Console.WriteLine("_______________________________");
+            Console.WriteLine("-------------------------------");
+            Console.Write(">>PALABRA MODIFICADA: ");
+            Console.WriteLine(objPalabra.Correccion_de_Caracteres(objPalabra.PALABRA));
+            Console.WriteLine("_______________________________");
+            Console.WriteLine("-------------------------------");
+            Console.ReadKey();
+        }
+    }
+}
