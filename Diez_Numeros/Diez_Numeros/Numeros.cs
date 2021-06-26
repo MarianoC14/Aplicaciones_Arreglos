9e236a99018bc645d95c5e9abcb540a9cb51b69d
MarianoC14 <86450013+MarianoC14@users.noreply.github.com>
2021-06-25 23:38:23 -0600

    Programa 10 Números
    Código del programa donde se utiliza un arreglo para almacenar 10 números para posteriormente promediar, sumar, indicar el número mayor y el menor entre estos.

---
+++ Diez_Numeros/Diez_Numeros/Numeros.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Diez_Numeros
+{
+    class Numeros
+    {
+        //Atributos 
+        double suma = 0, promedio, mayor = 0, menor;
+        //Atributos
+        public double SUMA
+        {
+            get { return this.suma; }
+            set { this.suma = value; }
+        }
+        public double PROMEDIO
+        {
+            get { return this.promedio; }
+            set { this.promedio = value; }
+        }
+        public double MENOR
+        {
+            get { return this.menor; }
+            set { this.menor = value; }
+        }
+        public double MAYOR
+        {
+            get { return this.mayor; }
+            set { this.mayor = value; }
+        }
+        //--------------Array-----------------------------
+        public double[] arrayNumeros = new double[10];
+        //-----------------Metodos-----------------------------
+        public void Numero_menor()
+        {
+            for (int i = 0; i <= 9; i++)//Ciclo Número Menor
+            {
+                if (arrayNumeros[i] < MENOR) { MENOR = arrayNumeros[i]; }//Condision para definir que numero es menor
+                else { MENOR = MENOR; }
+            }
+        }
+        public void Promedio()
+        {//-------------Obtencion del Promedio-----------------
+            PROMEDIO = SUMA / 10;}
+        public void Numero_Mayor()
+        {
+            Array.Sort(arrayNumeros);//Ordenar los números de Menor a mayor
+            MAYOR = arrayNumeros[9];//El numero mayor sera el ultimo numero ordenado
+        }
+    }
+}
---
+++ Diez_Numeros/Diez_Numeros/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Diez_Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numeros obj10Numeros = new Numeros();
+            //--------------Array-----------------------------
+            Console.WriteLine("<><><><><><DIEZ NUMEROS><><><><><><><>");
+            for (int x = 0; x <= 9; x++)
+            {
+                Console.Write("Escribe un valor: ");
+                obj10Numeros.arrayNumeros[x] = Convert.ToDouble(Console.ReadLine());
+                obj10Numeros.SUMA = obj10Numeros.SUMA + obj10Numeros.arrayNumeros[x];//Cada numero que se ingrese se agregara 
+                //--------Comparación del Número Mayor----------------            a la variable suma para de una sola accion ir sumando los numeros.
+                if (obj10Numeros.MAYOR > obj10Numeros.arrayNumeros[x])
+                { obj10Numeros.MAYOR = obj10Numeros.MAYOR; }
+                else {obj10Numeros.MAYOR = obj10Numeros.arrayNumeros[x]; }
+                //Console.WriteLine(arrayNumeros[x]);
+            }
+            obj10Numeros.MENOR = obj10Numeros.arrayNumeros[0];
+            obj10Numeros.Numero_menor();
+            //----------------------SALIDA----------------------------
+            Console.WriteLine("____________________________________");
+            Console.WriteLine($"---Numero Menor: {obj10Numeros.MENOR}");
+            obj10Numeros.Promedio();
+            Console.WriteLine("____________________________________");
+            Console.WriteLine($"---Promedio: {obj10Numeros.PROMEDIO}");
+            obj10Numeros.Numero_Mayor();
+            Console.WriteLine("____________________________________");
+            Console.WriteLine($"---Mayor: {obj10Numeros.MAYOR}");
+            Console.WriteLine("____________________________________");
+            Console.WriteLine($"---Suma: {obj10Numeros.SUMA}");
+            Console.WriteLine("FIN");
+        }
+    }
+}
