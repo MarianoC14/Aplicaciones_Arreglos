7528807fa4174f5f586f01e7499c465d8bbe3622
MarianoC14 <86450013+MarianoC14@users.noreply.github.com>
2021-06-26 00:02:43 -0600

    Programa Taller
    En este programa se utilizaron los Queue para representar la llegada de los autos al taller, para que cuando uno fuera atendido este abandonara la cola y otro pudiera ingresar, igualmente se utilizaron listas de Arrays para ir registrando cada auto atendido junto con su dueño y el cobro realizado donde serian 3 listas arrays (una para cada uno de los elementos mencionados).

---
+++ Taller/Taller/Taller_autos.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+
+namespace Taller
+{
+    class Taller_autos
+    {
+        ConsoleKeyInfo Opcion = new ConsoleKeyInfo();
+        public Queue<string> Autos = new Queue<string>();
+        public Queue<string> Propietarios = new Queue<string>();
+        public ArrayList Lista_Propietarios = new ArrayList();
+        public ArrayList Lista_Cobros = new ArrayList();
+        public ArrayList Lista_Autos = new ArrayList();
+        //------Arreglos
+        //public string[] Arrayplacas = new string[5];
+        //public string[] ArrayPropietario = new string[5];
+        //-----Atributos
+        int capacidad, autos_atendidos;
+        double cobro = 0;
+        //----Propiedades
+        public int CAPACIDAD
+        {
+            get { return this.capacidad; }
+            set { this.capacidad = value; }
+        }
+        public double COBRO
+        {
+            get { return this.cobro; }
+            set { this.cobro = value; }
+        }
+        public int AUTOS_ATENDIDOS
+        {
+            get { return this.autos_atendidos; }
+            set { this.autos_atendidos = value; }
+        }
+        //------Metodos
+        public void añadir_auto(string auto)
+        {
+            Autos.Enqueue(auto);
+        }
+        public void sacar_auto()
+        {
+            Autos.Dequeue();
+        }
+        public void Agregar_cobros(double pago)
+        {
+            Lista_Cobros.Add(pago);
+        }//--Añadir a mi lista de cobros
+        public void Agregar_Propietario(double persona)
+        {
+            Lista_Propietarios.Add(persona);
+        }//--Añadir a mi lista de propietarios
+        public void Agregar_Autos(double autos)
+        {
+            Lista_Autos.Add(autos);
+        }
+    }
+}
