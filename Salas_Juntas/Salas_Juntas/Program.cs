ebb2137148cb2ef9ba1a93bdae45985e136c9c95
MarianoC14 <86450013+MarianoC14@users.noreply.github.com>
2021-06-26 00:12:17 -0600

    Programa Salas_Juntas
    En este programa se utilizo un Arreglo bidimensional para hacer una representación de los días que son hábiles y las horas trabajadas diariamente. Este programa registra en cada uno de los días y horas si la sala esta ocupada o disponible para realizar juntas. El programa solo deja registrar una sala cuando esta se encuentra disponible y solo muestra la opción de utilizar dicha sala por 1, 2 o 3 horas.

---
+++ Salas_Juntas/Salas_Juntas/Program.cs
@@ -0,0 +1,185 @@
+﻿using System;
+
+namespace Salas_Juntas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string seleccion;
+            Salas_Reuniones objSala1 = new Salas_Reuniones();
+            Salas_Reuniones objSala2 = new Salas_Reuniones();
+            Salas_Reuniones objSala3 = new Salas_Reuniones();
+            objSala1.Todos_Disponibles();
+            objSala2.Todos_Disponibles();
+            objSala3.Todos_Disponibles();
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("AGENDA TU REUNION");
+                Console.WriteLine("SELECCIONA LA SALA QUE DESEAS RENTAR");//>>Menu de opciones
+                Console.WriteLine("1) -- SALA 1");
+                Console.WriteLine("2) -- SALA 2");
+                Console.WriteLine("3) -- SALA 3");
+                Console.WriteLine("4) -- SALIR DE LA APLICACION");
+                Console.WriteLine("______________________________");
+                Console.Write(">>Seleccion: ");
+                seleccion = Console.ReadLine().ToUpper();
+                switch (seleccion)
+                {
+                    case "1": //Sala 1
+                        {
+                            do
+                            {
+                                Console.Clear();
+                                Console.WriteLine("<<<SALA 1>>>");
+                                Console.WriteLine("--SELECCIONA EL DIA DE LA SEMANA QUE DESAS RENTAR--");//Menu de los dias que se pueden elegir
+                                Console.WriteLine("1) -- Lunes");
+                                Console.WriteLine("2) -- Martes");
+                                Console.WriteLine("3) -- Miercoles");
+                                Console.WriteLine("4) -- Jueves");
+                                Console.WriteLine("5) -- Viernes");
+                                Console.WriteLine("6) -- Sabado");
+                                Console.WriteLine("7) -- Domingo");
+                                Console.Write(">>Dia seleccionado: ");
+                                objSala1.DIA = Convert.ToInt32(Console.ReadLine());
+                                if (objSala1.DIA == 7)//Condision de si escoge el domingo. 
+                                { Console.WriteLine("---Este Dia no hay Servicio--"); Console.ReadKey();}//Mensaje de indicacion de que el domingo no hay servicio
+                            } while (objSala1.DIA == 7);//Ciclo pra que cuando escoja el domingo le vuelva a mostrar el menu. 
+                            do
+                            {
+                                Console.WriteLine("¿Ah que horas quieres agendar tu reunion? (Formato 24 hrs)");
+                                Console.Write(">>HORA: ");
+                                objSala1.HORA = Convert.ToInt32(Console.ReadLine());
+                                if (objSala1.Agenda[objSala1.DIA - 1, objSala1.HORA - 9] == ">>La Sala se Encuentra Ocupada<<")
+                                {
+                                    Console.WriteLine(">>ESA HORA YA ESTA OCUPADA");
+                                    Console.WriteLine();
+                                }//Condision por si la sala ya esta ocupada a la hora que esta ingresando el usuario. 
+                            } while (objSala1.Agenda[objSala1.DIA-1, objSala1.HORA - 9] == ">>La Sala se Encuentra Ocupada<<");
+                            if (objSala1.Agenda[objSala1.DIA - 1, objSala1.HORA - 9] != "Disponible") { Console.WriteLine("La sala no esta Disponible"); Console.ReadKey(); ; break; }
+                            objSala1.Hora_Reunion();
+                            Console.WriteLine(">> ¿CUANTAS HORAS QUIERES RENTAR ESTA SALA?");
+                            Console.WriteLine("----- 1 HORA ---- 2 HORAS ---- 3 HORAS ----");
+                            Console.WriteLine("--> HORAS: ");
+                            objSala1.HRS_REUNION = Convert.ToInt32(Console.ReadLine());
+                            objSala1.Hrs_Reunion();
+                        }
+                        break;
+                    case "2": //Sala 2
+                        {
+                            do
+                            {
+                                Console.Clear();
+                                Console.WriteLine("<<<SALA 1>>>");
+                                Console.WriteLine("--SELECCIONA EL DIA DE LA SEMANA QUE DESAS RENTAR--");
+                                Console.WriteLine("1) -- Lunes");
+                                Console.WriteLine("2) -- Martes");
+                                Console.WriteLine("3) -- Miercoles");
+                                Console.WriteLine("4) -- Jueves");
+                                Console.WriteLine("5) -- Viernes");
+                                Console.WriteLine("6) -- Sabado");
+                                Console.WriteLine("7) -- Domingo");
+                                Console.Write(">>Dia seleccionado: ");
+                                objSala2.DIA = Convert.ToInt32(Console.ReadLine());
+                                if (objSala2.DIA == 7)
+                                { Console.WriteLine("---Este Dia no hay Servicio--"); Console.ReadKey(); }
+                            } while (objSala2.DIA == 7);
+                            do
+                            {
+                                Console.WriteLine("¿Ah que horas quieres agendar tu reunion? (Formato 24 hrs)");
+                                Console.Write(">>HORA: ");
+                                objSala2.HORA = Convert.ToInt32(Console.ReadLine());
+                                if (objSala2.Agenda[objSala2.DIA-1, objSala2.HORA - 9] == ">>La Sala se Encuentra Ocupada<<")
+                                {
+                                    Console.WriteLine(">>ESA HORA YA ESTA OCUPADA");
+                                    Console.WriteLine();
+                                }
+                            } while (objSala2.Agenda[objSala2.DIA-1, objSala2.HORA - 9] == ">>La Sala se Encuentra Ocupada<<");
+                            if (objSala2.Agenda[objSala2.DIA - 1, objSala2.HORA - 9] != "Disponible") { Console.WriteLine("La sala no esta Disponible"); Console.ReadKey(); break; }
+                            objSala2.Hora_Reunion();
+                            Console.WriteLine(">> ¿CUANTAS HORAS QUIERES RENTAR ESTA SALA?");
+                            Console.WriteLine("----- 1 HORA ---- 2 HORAS ---- 3 HORAS ----");
+                            Console.Write("--> HORAS: ");
+                            objSala2.HRS_REUNION = Convert.ToInt32(Console.ReadLine());
+                            objSala2.Hrs_Reunion();
+                        }
+                        break;
+                    case "3": //Sala 3
+                        {
+                            do
+                            {
+                                Console.Clear();
+                                Console.WriteLine("<<<SALA 1>>>");
+                                Console.WriteLine("--SELECCIONA EL DIA DE LA SEMANA QUE DESAS RENTAR--");
+                                Console.WriteLine("1) -- Lunes");
+                                Console.WriteLine("2) -- Martes");
+                                Console.WriteLine("3) -- Miercoles");
+                                Console.WriteLine("4) -- Jueves");
+                                Console.WriteLine("5) -- Viernes");
+                                Console.WriteLine("6) -- Sabado");
+                                Console.WriteLine("7) -- Domingo");
+                                Console.Write(">>Dia seleccionado: ");
+                                objSala3.DIA = Convert.ToInt32(Console.ReadLine());
+                                if (objSala3.DIA == 7)
+                                { Console.WriteLine("---Este Dia no hay Servicio--"); Console.ReadKey(); }
+                            } while (objSala3.DIA == 7);
+                            do
+                            {
+                                Console.WriteLine("¿Ah que horas quieres agendar tu reunion? (Formato 24 hrs)");
+                                Console.Write(">>HORA: ");
+                                objSala3.HORA = Convert.ToInt32(Console.ReadLine());
+                                if (objSala3.Agenda[objSala3.DIA-1, objSala3.HORA - 9] == ">>La Sala se Encuentra Ocupada<<")
+                                {
+                                    Console.WriteLine(">>ESA HORA YA ESTA OCUPADA");
+                                    Console.WriteLine();
+                                }
+                            } while (objSala3.Agenda[objSala3.DIA-1, objSala3.HORA - 9] == ">>La Sala se Encuentra Ocupada<<");
+                            if (objSala3.Agenda[objSala3.DIA-1, objSala3.HORA - 9] != "Disponible") { Console.WriteLine("La sala no esta disponible"); Console.ReadKey(); ; break; }
+                            objSala3.Hora_Reunion();
+                            Console.WriteLine(">> ¿CUANTAS HORAS QUIERES RENTAR ESTA SALA?");
+                            Console.WriteLine("----- 1 HORA ---- 2 HORAS ---- 3 HORAS ----");
+                            Console.Write("--> HORAS: ");
+                            objSala3.HRS_REUNION = Convert.ToInt32(Console.ReadLine());
+                            objSala3.Hrs_Reunion();
+                        } break;
+                    case "REPORTE":
+                        {
+                            string dia = "";
+                            Console.Clear();
+                            Console.WriteLine("---HORARIOS DE LAS 3 SALAS---");
+                            for (int n = 1; n < 4; n++)//Ciclo para repetir 3 veces (en cada vuelta muestrara la respectiva sala 1, 2, 3)
+                            {
+                                Console.WriteLine("______________________________________________");
+                                Console.WriteLine($"----------------Sala {n}---------------------");
+                                Console.WriteLine("<><><><><><><><><><><><><><><><><><><><><><><>");
+                                for (int x = 0; x < 6; x++)//Filas (dias)
+                                {
+                                    switch (x)
+                                    {
+                                        case 0: dia = "Lunes"; break;
+                                        case 1: dia = "Martes"; break;
+                                        case 2: dia = "Miercoles"; break;
+                                        case 3: dia = "Jueves"; break;
+                                        case 4: dia = "Viernes"; break;
+                                        case 5: dia = "Sabado"; break;
+                                    }//Indicación de dias de la semana para mostrarlos. 
+                                    Console.WriteLine(dia);
+                                    for (int i = 0; i < 12; i++)//Columnas (horas)
+                                    {
+                                        switch(n)
+                                        {
+                                            case 1: Console.WriteLine($"---{i + 9}:00 >> {objSala1.Agenda[x,i]}");break;
+                                            case 2: Console.WriteLine($"---{i + 9}:00 >> {objSala2.Agenda[x,i]}");break;
+                                            case 3: Console.WriteLine($"---{i + 9}:00 >> {objSala3.Agenda[x,i]}");break;
+                                        }//Dependiendo en que vuelta (sala) vallamos. 
+                                    }
+                                }
+                            }
+                            Console.ReadKey();
+                        } break;
+                }
+            } while (seleccion != "4");//Ciclo para repetir el programa hasta que el usuario desee salir. 
+        }
+    }
+}
---
+++ Salas_Juntas/Salas_Juntas/Salas_Reuniones.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Salas_Juntas
+{
+    class Salas_Reuniones
+    {
+        //-----Arreglos
+        public string[,] Agenda = new string[6,12];
+        //-----Atributos
+        int dia;
+        int hora, hrs_reunion;
+        //----Propiedades
+        public int DIA
+        {
+            get { return this.dia;}
+            set { this.dia = value; }
+        }
+        public int HORA
+        {
+            get { return this.hora; }
+            set { this.hora = value; }
+        }
+        public int HRS_REUNION
+        {
+            get { return this.hrs_reunion; }
+            set { this.hrs_reunion = value; }
+        }
+        //------Métodos
+        public void Hora_Reunion()
+        {
+            if (Agenda[DIA - 1, HORA - 9] == "Disponible")//condision de si la sala esta disponible ahora ocuparla. 
+            {
+                Agenda[DIA - 1, HORA - 9] = ">>La Sala se Encuentra Ocupada<<";
+            }
+            else
+            {
+                Console.WriteLine(">>La sala se Encuentra Ocupada<<");//mensaje indicando que esta ocupada. 
+            }
+        }
+        public void Hrs_Reunion()
+        {
+            int[] Confirmacion = { 1, 1, 1,};
+            for (int i = 1; i <= HRS_REUNION; i++)
+            {
+                if (Agenda[DIA - 1, HORA - 9 + i] == "Disponible")//Condision de si la sala a la hora que esta indicando esta disponible me indique que si se puede asignar. 
+                {
+                    Confirmacion[i - 1] = 1;
+                }
+                else { Confirmacion[i - 1] = 0;}//Indicacion de que en el rango de horas hay una sala ocupada. 
+            }
+            if (Confirmacion[0] == 1 && Confirmacion[1] == 1 && Confirmacion[2] == 1)
+            {
+                for (int i = 1; i <= HRS_REUNION; i++)
+                {
+                    if (Agenda[DIA - 1, HORA - 9 + i] == "Disponible")//Asignar La sala como ocupada
+                    {
+                        Agenda[DIA - 1, HORA - 9 + i] = ">>La sala se Encuentra Ocupada<<";
+                    }
+                }
+            }
+            else//Si almenos en una de las horas hay una sala ocupada no hara nada.  
+            {
+                Console.WriteLine("En ese Rango de Horas hay Salas ocupadas");
+            }
+        }
+        public void Todos_Disponibles()
+        {
+            for(int x = 0; x < 6;x++)//Columnas (Dias)
+            {
+                for(int i = 0; i < 12;i++)//Filas (Horas) 
+                {
+                    Agenda[x, i] = "Disponible";
+                }
+            }
+        }//Este metodo definira que todas las salas estan disponibles al inicial el programa. 
+    }
+}
